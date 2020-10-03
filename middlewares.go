5a73c508ed9cd26f8f8ea82c53f65ac34023997a
b3b3r <b3b3rseg@gmail.com>
2020-10-03 21:43:55 +0200

    fix middleware

--- middlewares.go
+++ middlewares.go
@@ -7,7 +7,7 @@ import (
 
 func logRequest(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("[%v] %v", r.Method, r.RequestURI)
+		fmt.Printf("[%v] %v\n", r.Method, r.RequestURI)
 		next.ServeHTTP(w, r)
 	}
 }
