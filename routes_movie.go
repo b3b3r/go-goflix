91e30fcf9d5e2d323069afefcd465c6969270bb8
b3b3r <b3b3rseg@gmail.com>
2020-10-03 20:16:18 +0200

    create get movie list route

---
+++ routes_movie.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type jsonMovie struct {
+	ID          int64  `json:"id"`
+	Title       string `json:"title"`
+	ReleaseDate string `json:"release_date"`
+	Duration    int    `json:"duration"`
+	TrailerURL  string `json:"trailer_url"`
+}
+
+func mapMovieToJSON(m *Movie) jsonMovie {
+	return jsonMovie{
+		ID:          m.ID,
+		Title:       m.Title,
+		ReleaseDate: m.ReleaseDate,
+		Duration:    m.Duration,
+		TrailerURL:  m.TrailerURL,
+	}
+}
+
+func (s *server) handleMovieList() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		movies, err := s.store.GetMovies()
+		if err != nil {
+			log.Printf("Cannot load movies, err=%v\n", err)
+			s.respond(w, r, nil, http.StatusInternalServerError)
+			return
+		}
+		var resp = make([]jsonMovie, len(movies))
+		for i, m := range movies {
+			resp[i] = mapMovieToJSON(m)
+		}
+
+		s.respond(w, r, resp, http.StatusOK)
+	}
+}
