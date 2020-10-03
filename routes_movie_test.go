e2d7a6a967c5c6619b5c40b2f1faf544ddad2d72
b3b3r <b3b3rseg@gmail.com>
2020-10-03 22:20:33 +0200

    add test for create route

---
+++ routes_movie_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testStore struct {
+	movieID int64
+	movies  []*Movie
+}
+
+func (t testStore) Open() error {
+	return nil
+}
+
+func (t testStore) Close() error {
+	return nil
+}
+
+func (t testStore) GetMovies() ([]*Movie, error) {
+	return t.movies, nil
+}
+
+func (t testStore) GetMovieByID(id int64) (*Movie, error) {
+	for _, m := range t.movies {
+		if m.ID == id {
+			return m, nil
+		}
+	}
+	return nil, nil
+}
+
+func (t testStore) CreateMovie(m *Movie) error {
+	t.movieID++
+	m.ID = t.movieID
+	t.movies = append(t.movies, m)
+	return nil
+}
+
+func TestMovieCreateUnit(t *testing.T) {
+	srv := newServer()
+	srv.store = &testStore{}
+
+	p := struct {
+		Title       string `json:"title"`
+		ReleaseDate string `json:"release_date"`
+		Duration    int    `json:"duration"`
+		TrailerURL  string `json:"trailer_url"`
+	}{
+		Title:       "Coucou",
+		ReleaseDate: "1984",
+		Duration:    35,
+		TrailerURL:  "coucou",
+	}
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(p)
+	assert.Nil(t, err)
+
+	r := httptest.NewRequest("POST", "/api/movies/", &buf)
+	w := httptest.NewRecorder()
+
+	srv.handleMovieCreate()(w, r)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
